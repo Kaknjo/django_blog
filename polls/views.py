4a4d5e6f63f91338de67d05c6fc036f8caa9fd2f
Kaknjo <harun@eacon.ba>
2019-06-13 12:12:04 -0700

    Views bez templatea

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,22 @@
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+	# Srtiramo u niz od 5 clanova po datumu objavljivanja pitanja i ispisujemo ih for petljom 
+
+    lista_zadnjih_pitanja=Question.objects.order_by('-pub_date')[:5]
+    izlaz=','.join([q.question_text for q in lista_zadnjih_pitanja])
+    return HttpResponse(izlaz)
+
+def detail(request, question_id):
+	return HttpResponse("Pitanje je %s" % question_id)
+
+def result(request, question_id):
+	response="Odgovor na pitanje je %s"
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("Glasali ste na pitanje" % question_id)
+
